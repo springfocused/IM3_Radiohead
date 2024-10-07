89539d685ae30403f544fff9885c54e8c02d65b0
Suenaf <suena.fischer@stud.fhgr.ch>
2024-10-07 14:17:37 +0200

    etl - config.php

---
+++ etl/config.php
@@ -0,0 +1,19 @@
+<?php
+
+// Datenbankverbindungsparameter
+$host = 'bf6o9g.myd.infomaniak.com';
+$dbname = 'bf609g_im3';
+$username = 'bf609g_im3';
+$password = 'Z60J_64gyS0';
+
+// DSN (Datenquellenname) für PDO
+$dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
+
+// Optionen für PDO
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Aktiviert die Ausnahmebehandlung für Datenbankfehler
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Legt den Standard-Abrufmodus auf assoziatives Array fest
+    PDO::ATTR_EMULATE_PREPARES => false, // Deaktiviert die Emulation vorbereiteter Anweisungen, für bessere Leistung
+];
+
+?>
